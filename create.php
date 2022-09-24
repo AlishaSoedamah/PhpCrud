847f2638db25fc5f9a5019f51978c6203571f00d
=Alisha Soedamah <alisha.soedamah@gmail.com>
2022-09-24 14:17:06 +0200

    Create done

--- create.php
+++ create.php
@@ -1,44 +1,48 @@
 
  <link rel="stylesheet" href="style.css">
- <?php include 'databaseConnect.php'; 
- $pdo = pdo_connect_mysql();
- $msg = '';
- // Check if POST data is not empty
- if (!empty($_POST)) {
-     // Post data not empty insert a new record
-     // Set-up the variables that are going to be inserted, we must check if the POST variables exist if not we can default them to blank
-     $id = isset($_POST['id']) && !empty($_POST['id']) && $_POST['id'] != 'auto' ? $_POST['id'] : NULL;
-     // Check if POST variable "name" exists, if not default the value to blank, basically the same for all variables
-     $naam =isset($_POST['naam']) ? $_POST['naam'] : '';
-     $klas = isset($_POST['klas']) ? $_POST['klas'] : '';
-     $aantalMin = isset($_POST['aantal min']) ? $_POST['aantal min'] : '';
-     $reden = isset($_POST['reden']) ? $_POST['reden'] : '';
-     
-     // Insert new record into the contacts table
-     $DATABASE_HOST = 'localhost';
-     $DATABASE_USER = 'root';
-     $DATABASE_PASS = '';
-     $DATABASE_NAME = 'myphpcrud';
- 
-     $link = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
- 
-     $statement = $link->prepare('INSERT INTO testtable (name, lastname, age)
-         VALUES (:fname, :sname, :age)');
- 
-     $statement->execute([
-         'fname' => 'Bob',
-         'sname' => 'Desaunois',
-         'age' => '18',
-     ]);
-     // Output message
-     $msg = 'Created Successfully!';
- }
+<?php
+include 'databaseConnect.php'; 
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check if POST data is not empty
+if (!empty($_POST)) {
+    // Post data not empty insert a new record
+    // Set-up the variables that are going to be inserted, we must check if the POST variables exist if not we can default them to blank
+    $id = isset($_POST['id']) && !empty($_POST['id']) && $_POST['id'] != 'auto' ? $_POST['id'] : NULL;
+    // Check if POST variable "name" exists, if not default the value to blank, basically the same for all variables
+    $naam = isset($_POST['naam']) ? $_POST['naam'] : '';
+    $klas = isset($_POST['klas']) ? $_POST['klas'] : '';
+    $mins = isset($_POST['aantal min']) ? $_POST['aantal min'] : '';
+    $reden = isset($_POST['reden']) ? $_POST['reden'] : '';
+
+    // Insert new record into the studenten table
+    $stmt = $pdo->prepare('INSERT INTO studenten VALUES (?, ?, ?, ?, ?)');
+    $stmt->execute([$id, $naam, $klas, $mins, $reden]);
+    // Output message
+    $msg = 'Created Successfully!';
+}
+
+$dbhost = 'localhost';
+$dbname = 'myphpcrud';
+$dbusername = 'root';
+$dbpassword = '';
+
+$link = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
+
+$statement = $link->prepare('INSERT INTO testtable (name, lastname, age)
+    VALUES (:fname, :sname, :age)');
+
+$statement->execute([
+    'fname' => 'Bob',
+    'sname' => 'Desaunois',
+    'age' => '18',
+]);
 
 //  if ($aantalMin < 0) {
 //     return "Vul een geldige tijd in!";
 //  }
 
- ?>
+?>
 
  <nav>
     <a href="read.php">Studenten</a>
@@ -47,15 +51,17 @@
 
 <div class="create">
 <h1>Create</h1>
-    <form action="create.php" method="get">
+    <form action="create.php" method="post">
         <label for="id">ID</label>
         <input type="text" name="id" placeholder="26" value="auto" id="id">
         <label for="naam">Naam</label>
-        <input type="text" name="name" placeholder="John Doe" id="name">
+        <input type="text" name="naam" placeholder="John Doe" id="naam">
+        <label for="klas">Klas</label>
+        <input type="text" name="klas" placeholder="4c" id="klas">
         <label for="reden">Reden</label>
         <input type="text" name="reden" placeholder="reden" id="reden">
-        <label for="reden">Min te laat</label>
-        <input type="number" name="min" placeholder="min te laat" id="min">
+        <label for="aantal min">Min te laat</label>
+        <input type="number" name="aantal min" placeholder="min laat" id="aantal min">
         <input type="submit" class="add-student" value="Zet student erbij"></input>
     </form>
     <?php if ($msg) : ?>
--- update.php
+++ update.php
@@ -1,15 +1,32 @@
- <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css">
  <?php include 'databaseConnect.php'; 
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check if the contact id exists, for example update.php?id=1 will get the contact with the id of 1
+if (isset($_GET['id'])) {
+    if (!empty($_POST)) {
+        // This part is similar to the create.php, but instead we update a record and not insert
+        $id = isset($_POST['id']) ? $_POST['id'] : NULL;
+        $name = isset($_POST['name']) ? $_POST['name'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+        $title = isset($_POST['title']) ? $_POST['title'] : '';
+        // Update the record
+        $stmt = $pdo->prepare('UPDATE contacts SET id = ?, name = ?, email = ?, phone = ?, title = ?, created = ? WHERE id = ?');
+        $stmt->execute([$id, $name, $email, $phone, $title, $created, $_GET['id']]);
+        $msg = 'Updated Successfully!';
+    }
+        // Get the contact from the contacts table
+        $stmt = $pdo->prepare('SELECT * FROM contacts WHERE id = ?');
+        $stmt->execute([$_GET['id']]);
+        $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$contact) {
+            exit('Contact doesn\'t exist with that ID!');
+        }
+    } else {
+        exit('No ID specified!');
+    }
 
-     // Validate name
-     $input_name = trim($_POST["name"]);
-     if(empty($input_name)){
-         $name_err = "Please enter a name.";
-     } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-         $name_err = "Please enter a valid name.";
-     } else{
-         $name = $input_name;
-     }
  ?>
 
  <nav>
@@ -26,7 +43,7 @@
         <input type="text" name="name" placeholder="John Doe" id="name">
         <label for="reden">Reden</label>
         <input type="text" name="reden" placeholder="reden" id="reden">
-        <label for="reden">Min te laat</label>
+        <label for="min">Min te laat</label>
         <input type="number" name="min" placeholder="min te laat" id="min">
         <button class="add-student" href="/">Update deze student</button>
     </form>
